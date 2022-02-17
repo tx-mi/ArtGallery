977ddf62834569f0d66acd207c45503eab243864
Ramazan <txmiwxxd@gmail.com>
2022-02-17 23:02:17 +0300

    Add title for favourites

--- Art Gallery/Art Gallery/Controllers/Favourites/FavouritesTableViewController.swift
+++ Art Gallery/Art Gallery/Controllers/Favourites/FavouritesTableViewController.swift
@@ -14,6 +14,7 @@ class FavouritesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Favourites"
         
         setupTableView()
     }
