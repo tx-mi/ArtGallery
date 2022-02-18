c28d7a6a77ca3e188e586a26686acf06d125b2a5
Ramazan <txmiwxxd@gmail.com>
2022-02-18 19:54:08 +0300

    add access key

---
+++ Art Gallery/Art Gallery/Models/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  Art Gallery
+//
+//  Created by Ramazan Abdulaev on 16.02.2022.
+//
+
+import Foundation
+
+struct Keys {
+    let accessKey = "ppwxDzRaEVaeM_gYlSn4cv1UC9eZYwVaEYyYFtW0Tek"
+    let secretKey = "it is secret man"
+}
